f84bd531bc0959eaa0741e763a97ae1067bbd357
VukDjedovic <vuk.djedovic@silaris.rs>
2023-01-28 03:16:53 +0100

    Završen projekat

--- Order.Domain/Services/OrderService.cs
+++ Order.Domain/Services/OrderService.cs
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
 ﻿
 using Order.Domain.Helper;
 using Order.Domain.Interfaces;
-=======
-﻿using Order.Domain.Interfaces;
->>>>>>> dokumentovanje
-using Order.Domain.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace Order.Domain.Services
 {
