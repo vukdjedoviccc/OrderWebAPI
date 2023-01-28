bb9a597972b5c6ce60e67397cd85961de51ccc23
VukDjedovic <vuk.djedovic@silaris.rs>
2023-01-28 01:43:01 +0100

    Dodate su testne klase u okviru projekta OrderWebAPITest

---
+++ OrderWebAPITest/DomainValidationTests/CompanyTests.cs
@@ -0,0 +1,119 @@
+﻿using Order.Domain.DomainValidations;
+using Order.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OrderWebAPITest.DomainValidationTests
+{
+    public class CompanyTests
+    {
+        #region ValidCompanyCreatedTest
+        [Fact]
+        public void Valid_Company_Created() 
+        {
+            // Arrange and act
+            Company company = new Company("Silaris Solutions", "91345287", "Jurija Gagarina14G, Beograd", "0645233699", "silaris@gmail.com", 1);
+            
+            // Assert
+            Assert.NotNull(company);
+            Assert.NotEmpty(company.FullName);
+            Assert.NotEmpty(company.RegistrationNumber);
+            Assert.NotEmpty(company.Adress);
+            Assert.NotEmpty(company.PhoneNumber);
+            Assert.NotEmpty(company.Email);
+            Assert.NotNull(company.FullName);
+            Assert.NotNull(company.RegistrationNumber);
+            Assert.NotNull(company.Adress);
+            Assert.NotNull(company.PhoneNumber);
+            Assert.NotNull(company.Email);
+        }
+        #endregion
+
+        #region FullNameTests
+        [Theory]
+        [InlineData(null, "12345678", "Vojvode Stepe 174, Beograd", "0652193578", "test@gmail.com", 12)]
+        public void ArgumentNullException_Thrown_When_FullName_Is_Null(string fullName, 
+            string registrationNumber, 
+            string adress, 
+            string phoneNumber, 
+            string email,
+            int id) 
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new Company(fullName, registrationNumber, adress, phoneNumber, email, id));
+        }
+
+        [Theory]
+        [InlineData("", "12345678", "Vojvode Stepe 174, Beograd", "0652193578", "test@gmail.com", 12)]
+        public void ArgumentException_Thrown_When_FullName_Is_Empty(string fullName,
+            string registrationNumber,
+            string adress,
+            string phoneNumber,
+            string email,
+            int id)
+        {
+            // Assert
+            Assert.Throws<ArgumentException>(() => new Company(fullName, registrationNumber, adress, phoneNumber, email, id));
+        }
+
+        [Theory]
+        [InlineData("Silaris Solutionsqwertzuioplskdhdkksbssdshdhdhfhiwdjqvdqwdqwdvjdqwwiohffdbfwefofwefiwefhwiefwefhfewfw", 
+            "123456789", "Vojvode Stepe 174, Beograd", "0652193578", "test@gmail.com", 12)]
+        public void ArgumentException_Thrown_When_FullName_Length_Is_More_Than_50_Characters(string fullName,
+            string registrationNumber,
+            string adress,
+            string phoneNumber,
+            string email,
+            int id)
+        {
+            // Assert
+            Assert.Throws<ArgumentException>(() => new Company(fullName, registrationNumber, adress, phoneNumber, email, id));
+        }
+        #endregion
+
+        #region RegistrationNumberTests
+        [Theory]
+        [InlineData("Silaris Solutions", null, "Vojvode Stepe 174, Beograd", "0652193578", "test@gmail.com", 12)]
+        public void ArgumentNullException_Thrown_When_RegistrationNumber_Is_Null(string fullName,
+            string registrationNumber,
+            string adress,
+            string phoneNumber,
+            string email,
+            int id)
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => new Company(fullName, registrationNumber, adress, phoneNumber, email, id));
+        }
+
+        [Theory]
+        [InlineData("Silaris Solutions", "", "Vojvode Stepe 174, Beograd", "0652193578", "test@gmail.com", 12)]
+        public void ArgumentException_Thrown_When_RegistrationNumber_Is_Empty(string fullName,
+            string registrationNumber,
+            string adress,
+            string phoneNumber,
+            string email,
+            int id)
+        {
+            // Assert
+            Assert.Throws<ArgumentException>(() => new Company(fullName, registrationNumber, adress, phoneNumber, email, id));
+        }
+
+        [Theory]
+        [InlineData("Silaris Solutions", "123456789", "Vojvode Stepe 174, Beograd", "0652193578", "test@gmail.com", 12)]
+        public void ArgumentException_Thrown_When_RegistrationNumber_Length_Is_More_Than_8_Characters(string fullName,
+            string registrationNumber,
+            string adress,
+            string phoneNumber,
+            string email,
+            int id)
+        {
+            // Assert
+            Assert.Throws<ArgumentException>(() => new Company(fullName, registrationNumber, adress, phoneNumber, email, id));
+        }
+        #endregion
+    }
+}
---
+++ OrderWebAPITest/DomainValidationTests/OrderItemTests.cs
@@ -0,0 +1,161 @@
+﻿using Order.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OrderWebAPITest.DomainValidationTests
+{
+    public class OrderItemTests
+    {
+        #region ValidorderItemCreatedTest
+        [Fact]
+        public void Valid_OrderItem_Created()
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act
+            OrderItem orderItem = new OrderItem(1, 12, 5480, 7, product);
+
+            // Assert
+            Assert.NotNull(orderItem);
+            Assert.NotNull(orderItem.Product);
+        }
+        #endregion
+
+        #region IdTests
+        [Theory]
+        [InlineData(null, 12, 6700.88, 1)]
+        public void ArgumentException_Thrown_When_Id_Is_Null(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+
+        [Theory]
+        [InlineData(-1, 12, 6700.88, 1)]
+        public void ArgumentException_Thrown_When_Id_Is_Negative(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+
+        [Theory]
+        [InlineData(0, 12, 6700.88, 1)]
+        public void ArgumentException_Thrown_When_Id_Is_0(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+        #endregion
+
+        #region QuantityTests
+        [Theory]
+        [InlineData(1, -12, 6700.88, 1)]
+        public void ArgumentException_Thrown_When_Quantity_Is_Negative(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+
+        [Theory]
+        [InlineData(1, 0, 6700.88, 1)]
+        public void ArgumentException_Thrown_When_Quantity_Is_0(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+        #endregion
+
+        #region AmountTests
+        [Theory]
+        [InlineData(1, 12, -6700.88, 1)]
+        public void ArgumentException_Thrown_When_Amount_Is_Negative(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+
+        [Theory]
+        [InlineData(1, 20, 0, 1)]
+        public void ArgumentException_Thrown_When_Amount_Is_0(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+        #endregion
+
+        #region ProductIdTests
+        [Theory]
+        [InlineData(1, 12, 6700.88, null)]
+        public void ArgumentException_Thrown_When_ProductId_Is_Null(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+
+        [Theory]
+        [InlineData(1, 12, 6700.88, -1)]
+        public void ArgumentException_Thrown_When_ProductId_Is_Negative(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+
+        [Theory]
+        [InlineData(1, 12, 6700.88, 0)]
+        public void ArgumentException_Thrown_When_ProductId_Is_0(int id, int quantity, decimal amount, int productId)
+        {
+            // Arrange
+            Product product = new Product(1, "Jakna", 8799, 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => new OrderItem(id, quantity, amount, productId, product));
+        }
+        #endregion
+
+        #region ProductTests
+        [Theory]
+        [InlineData(1, 1)]
+        public void ArgumentNullException_Thrown_When_Product_Is_Null(int productId, int promotionId)
+        {
+            // Arrange
+            Product? product = null;
+            Promotion promotion = new Promotion(1, "Zimska", 17);
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => new PromotionProduct(productId, promotionId, product, promotion));
+        }
+        #endregion
+    }
+}
